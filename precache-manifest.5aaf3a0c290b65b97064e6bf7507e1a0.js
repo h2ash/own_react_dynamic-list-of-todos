39254b2e9a47b51380a34c918a4242dfffbcdd32
h2ash <hun22ash@gmail.com>
2019-07-08 16:55:37 +0300

    Updates

---
+++ precache-manifest.5aaf3a0c290b65b97064e6bf7507e1a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fc661723161c77de5b4941f0c124c2b",
+    "url": "/own_react_dynamic-list-of-todos/index.html"
+  },
+  {
+    "revision": "9abd21ba424913b9624f",
+    "url": "/own_react_dynamic-list-of-todos/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/own_react_dynamic-list-of-todos/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "9abd21ba424913b9624f",
+    "url": "/own_react_dynamic-list-of-todos/static/js/main.9530cde7.chunk.js"
+  },
+  {
+    "revision": "aaa946a064306ebb17f3",
+    "url": "/own_react_dynamic-list-of-todos/static/js/runtime~main.be3a6ccd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/own_react_dynamic-list-of-todos/static/media/logo.5d5d9eef.svg"
+  }
+]);
